499da9a71c29099c49e855ec6109c80ff4b4c611
KennySprings4Life <ryan.a.ladwig@gmail.com>
2016-05-18 22:39:49 -0700

    Update game.js

--- game.js
+++ game.js
@@ -65,13 +65,12 @@ fireIcon.position.y = 9;
 
 wandContainer.addChild(fireIcon);
 
-
-
 var hero = new PIXI.Sprite.fromImage("hero.png");
 hero.anchor.x = 0.5;
 hero.anchor.y = 0.5;
 hero.position.x = 130;
 hero.position.y = 250;
+hero.health = 250;
 hero.attackType = "null";
 hero.enemy = "null";
 stage.addChild(hero);
@@ -94,6 +93,7 @@ evilEyeBody.anchor.y = 0.5;
 evilEyeBody.position.x = 400;
 evilEyeBody.position.y = 280;
 evilEyeBody.health = 100;
+monsterContainer.one = true;
 monsterContainer.addChild(evilEyeBody);
 
 //Generate the Eye of the 'Eye Monster' seperately so that the eye can be animated later.
@@ -111,6 +111,7 @@ evilHead.anchor.y = 0;
 evilHead.position.x = 400;
 evilHead.position.y = 180;
 evilHead.health = 100;
+monsterContainer.two = true;
 monsterContainer.addChild(evilHead);
 
 var evilWheel = new PIXI.Sprite.fromImage("WheelRuin-50px.png");
@@ -120,29 +121,8 @@ evilWheel.anchor.y = 0.5;
 evilWheel.position.x = 500;
 evilWheel.position.y = 250;
 evilWheel.health = 100;
+monsterContainer.three = true;
 monsterContainer.addChild(evilWheel);
-
-
-var healthBars = new PIXI.Container();
-healthBars.position.x = 250;
-healthBars.position.y = -75;
-
-var wheelHealth = new PIXI.Text("Wheel: "+evilWheel.health, {fill: 0xFFFF00, font: 'bold 15px Arial'});
-wheelHealth.anchor.x = 0;
-wheelHealth.anchor.y = 0;
-healthBars.addChild(wheelHealth);
-
-var headHealth = new PIXI.Text(evilHead.health, {fill: 0xFFFF00});
-headHealth.anchor.x = 0;
-headHealth.anchor.y = 0;
-healthBars.addChild(headHealth);
-
-var eyeHealth = new PIXI.Text(evilEye.health, {fill: 0xFFFF00});
-eyeHealth.anchor.x = 0;
-eyeHealth.anchor.y = 0;
-healthBars.addChild(eyeHealth);
-
-optionsBarSprite.addChild(healthBars);
 
 //Attack Icon Mouse Handlers
 function mouseHandlerSwordIcon(e) {
@@ -175,63 +155,80 @@ function attack(){
 	if(hero.attackType == "sword"){
 		if(hero.enemy == evilWheel){
 			evilWheel.health -= 5;
-			window.alert(evilWheel.health);
+			//window.alert(evilWheel.health);
 		}else if(hero.enemy == evilHead){
 			evilHead.health -= 45;
-			window.alert(evilHead.health);
+			//window.alert(evilHead.health);
 		}else{		//hero.enemy == evilEye;
 			evilEyeBody.health -= 10;
-			window.alert(evilEyeBody.health);
+			//window.alert(evilEyeBody.health);
 		}
 	}else if(hero.attackType == "fire"){
 		if (hero.enemy == evilWheel){
 			evilWheel.health -= 45;
-			window.alert(evilWheel.health);
+			//window.alert(evilWheel.health);
 		}else if(hero.enemy == evilHead){
 			evilHead.health -= 15;
-			window.alert(evilHead.health);
+			//window.alert(evilHead.health);
 		}else{//hero.enemy == evilEye;
 			evilEyeBody.health += 20;
-			window.alert(evilEyeBody.health);
+			//window.alert(evilEyeBody.health);
 		}
 	}else{//hero.attackType == water;
 		if(hero.enemy == evilWheel){
 			evilWheel.health -= 10;
-			window.alert(evilWheel.health);
+			//window.alert(evilWheel.health);
 		}else if(hero.enemy == evilHead){
 			evilHead.health += 20;
-			window.alert(evilHead.health);
+			//window.alert(evilHead.health);
 		}else{		//hero.enemy == evilEye;
 			evilEyeBody.health = -45;
-			window.alert(evilEyeBody.health);
+			//window.alert(evilEyeBody.health);
 		}
 	}
-	if(evilEyeBody.health <= 0){
+	window.alert("Eye Slug: "+evilEyeBody.health + " Evil Wheel: "+evilWheel.health+" Flying Head: "+ evilHead.health);
+	if(evilEyeBody.health <= 0 && monsterContainer.one == true){
+		monsterContainer.numMonsters -= 1;
+		monsterContainer.one = false;
 		evilEyeBody.destroy();
 		monsterContainer.removeChild(evilEyeBody);
 	}
-	if(evilHead.health <= 0){
+	if(evilHead.health <= 0 && monsterContainer.two == true){
+		monsterContainer.numMonsters -= 1;
+		monsterContainer.two = false;
 		evilHead.destroy();
 		monsterContainer.removeChild(evilHead);
 	}
-	if(evilWheel.health <= 0){
+	if(evilWheel.health <= 0 && monsterContainer.three == true){
+		monsterContainer.numMonsters -= 1;
+		if(monsterContainer.numMonsters == 0){
+			stage.destroy();
+			window.alert("You have won!");
+			return;
+		}
+		monsterContainer.three = false;
 		evilWheel.destroy();
 		monsterContainer.removeChild(evilWheel);
 	}
 	if(monsterContainer.numMonsters <= 0){
-		//Win
+		window.alert("You have won!");
+		stage.destroy();
+		
 	}else{
 		hero.health -= 7*monsterContainer.numMonsters;
+		if(hero.health <= 0){
+			window.alert("You have lost!");
+			stage.destroy();
+		}
 	}
+	window.alert("Your Health: "+hero.health);
 }
 
 
 
 function mouseHandlerEye(e) {
 	hero.enemy = evilEyeBody;
-	//window.alert(hero.attackType);
 	if(hero.attackType != "null"){
-		backWallSprite.visible = false;
 		attack();
 	}
 	
@@ -268,6 +265,9 @@ function animateHero(direction) {
 }
 
 function animate() {
+	if(monsterContainer.three == false){
+		return;	//An attemp to figure out where the program was crashing
+	}
 	requestAnimationFrame(animate);
 	evilWheel.rotation += 0.01;
 	renderer.render(stage);
